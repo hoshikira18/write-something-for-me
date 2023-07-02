310bb370e9fb1bcf3d1d7cd47cb2e19a61b46706
tranvankhuyen <khuyendvdtht@gmail.com>
2023-07-02 09:09:33 +0700

    feat: err alert

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,10 @@ import type { V2_MetaFunction } from '@remix-run/node';
 import handleSendMessage from '../javascripts/handleSendMessage';
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: 'Ship tool' }, { className: 'description', content: 'Welcome to Remix!' }];
+  return [
+    { title: 'Những điều thầm kín' },
+    { className: 'description', content: 'Nơi thổ lộ tâm tình' }
+  ];
 };
 
 export default function Index() {
@@ -39,13 +42,20 @@ export default function Index() {
                     .then((message) => {
                       console.log('Created message:', message);
                       document.getElementById('comment').value = '';
-                      document.getElementById('alert').classList.replace('hidden', 'flex');
+                      document.getElementById('alert-success').classList.replace('hidden', 'flex');
                       setTimeout(() => {
-                        document.getElementById('alert').classList.replace('flex', 'hidden');
+                        document
+                          .getElementById('alert-success')
+                          .classList.replace('flex', 'hidden');
                       }, 5000);
                     })
                     .catch((error) => {
                       console.error('Error:', error);
+                      document.getElementById('comment').value = '';
+                      document.getElementById('alert-err').classList.replace('hidden', 'flex');
+                      setTimeout(() => {
+                        document.getElementById('alert-err').classList.replace('flex', 'hidden');
+                      }, 5000);
                     });
                 }}>
                 Send me
@@ -54,7 +64,7 @@ export default function Index() {
           </div>
         </form>
         <div
-          id="alert"
+          id="alert-success"
           className="mb-4 hidden rounded-lg bg-green-50 p-4 text-sm text-green-800 dark:bg-gray-800 dark:text-green-400"
           role="alert">
           <svg
@@ -72,6 +82,26 @@ export default function Index() {
           <div>
             <span className="font-medium">Khuyến nhận được rồi nhé!</span> Chúc bạn một ngày tuyệt
             vời.
+          </div>
+        </div>
+        <div
+          id="alert-err"
+          className=" mb-4 hidden rounded-lg bg-red-50 p-4 text-sm text-red-800 dark:bg-gray-800 dark:text-red-400"
+          role="alert">
+          <svg
+            aria-hidden="true"
+            className="mr-3 inline h-5 w-5 flex-shrink-0"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"></path>
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>
+            <span className="font-medium">Ohhh!</span> Kiểm tra lại mạng nhé bạn ơi.
           </div>
         </div>
         <p className="mb-0.5 ml-auto text-xs text-gray-500 dark:text-gray-400">
